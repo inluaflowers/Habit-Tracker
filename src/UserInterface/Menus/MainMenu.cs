85a72447e95eb137990038bb52964a6d70dbd697
[Derek Wisher] <[inluawisher@gmail.com]>
2025-09-12 08:44:26 -0700

    Making Progress

---
+++ src/UserInterface/Menus/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿namespace UserInterface.Menus;
+
+public class MainMenu : Menu
+{
+    private const string Title = "LMer\nShhh! Be vewy, vewy quiet. We're tracking habits!\nMain Menu";
+    private static readonly Selection[] MenuSelections = [new Selection(), new Selection(), new Selection()];
+    public UserInterface MenuUi = new(Title, MenuSelections);
+    public override Selection SetActive()
+    {
+        return MenuUi.Show();
+
+    }
+}
+
---
+++ src/UserInterface/Selection.cs
@@ -0,0 +1,15 @@
+﻿namespace UserInterface;
+public class Selection
+{
+    private string Name { get; set;}
+    public Selection(string name)
+    {
+        Name = name;
+    }
+
+    public override string ToString()
+    {
+        return Name;
+    }
+}
+
